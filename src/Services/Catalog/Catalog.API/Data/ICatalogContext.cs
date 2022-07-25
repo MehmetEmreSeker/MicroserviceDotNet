5a0d87d1e5753074cfb1de43e6fee78eacd741e9
Mehmet Emre Şeker <mseker@ford.com.tr>
2022-07-25 15:05:58 +0300

    Catalog API was completed and Basket API was created

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        //basicly stores the products collection of the mongodb
+        IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories.ProductRepository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductByName(string name);
+        Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task DeleteProduct(string id);
+    }
+}
